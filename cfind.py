a870f41651239b1942cf176feee7fa2e2679e1f9
Mike Kang <mikekang@obigo.(none)>
2013-10-18 19:20:33 +0900

    fixed the path bug

--- cfind.py
+++ cfind.py
@@ -1,22 +1,34 @@
 import sys
 import os 
+import re
+
+ignores_gyp =[ './third_party/android_tools', './tools/gyp/test/mac']
 
 def travel_gyp(repath):
-  print 'travel ' + repath
+  #print 'travel ' + repath
   dirs = []
   for f in os.listdir(repath):
     #print f
-    if os.path.islink(f):
+    path = os.path.join(repath, f)
+    if os.path.islink(path):
       continue
-    if os.path.isdir(f):
+    if os.path.isdir(path):
+      bfind = False
+      for i in ignores_gyp:
+        if i == path:
+          bfind = True
+      if bfind:
+        continue
       if f != '.git' and f != '.svn':
         dirs.append(f)
-    elif os.path.isfile(f):
-      print f
+    elif os.path.isfile(path):
+      #print path
+      if re.match('.*\.gypi?', f) != None:
+        print path
 
   for d in dirs:
-    travel_gyp(os.path.join(repath + d))
+    travel_gyp(os.path.join(repath, d))
 
-def find():
-  travel_gyp(os.getcwd())
+def find(path='.'):
+  travel_gyp(path)
  
